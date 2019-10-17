0b6f7d64d21956cbcde253cc86889d5286324350
venkateshur <40040164+venkateshur@users.noreply.github.com>
2019-10-17 16:40:24 +0530

    Update FileWriter.py
    added compression

--- FileWriter.py
+++ FileWriter.py
@@ -1,29 +1,29 @@
 #File writer - write files of format csv,json,xml,parquet,text
 
-def csvWriter(processedDf, outputPath, mode ='overwrite', *paritionColumns):
+def csvWriter(processedDf, outputPath, mode ='overwrite', codec = None, *paritionColumns):
     if requiredPartitions(paritionColumns):
-        processedDf.write.mode(mode).partitionBy(*paritionColumns).csv(outputPath)
+        processedDf.write.mode(mode).partitionBy(*paritionColumns).csv(outputPath, compression = codec)
     else:
         processedDf.write.mode(mode).csv(outputPath)
 
 
 def jsonWriter(processedDf, outputPath, mode ='overwrite', *paritionColumns):
     if requiredPartitions(paritionColumns):
-        processedDf.write.mode(mode).partitionBy(*paritionColumns).json(outputPath)
+        processedDf.write.mode(mode).partitionBy(*paritionColumns).json(outputPath, compression = codec)
     else:
         processedDf.write.mode(mode).json(outputPath)
 
 
 def parquetWriter(processedDf, outputPath, mode ='overwrite', *paritionColumns):
     if requiredPartitions(paritionColumns):
-        processedDf.write.mode(mode).partitionBy(*paritionColumns).parquet(outputPath)
+        processedDf.write.mode(mode).partitionBy(*paritionColumns).parquet(outputPath, compression = codec)
     else:
         processedDf.write.mode(mode).parquet(outputPath)
 
 
 def text(processedDf, outputPath, mode ='overwrite', *paritionColumns):
     if requiredPartitions(paritionColumns):
-        processedDf.write.mode(mode).partitionBy(*paritionColumns).text(outputPath)
+        processedDf.write.mode(mode).partitionBy(*paritionColumns).text(outputPath, compression = codec)
     else:
         processedDf.write.mode(mode).text(outputPath)
 
@@ -31,7 +31,7 @@ def text(processedDf, outputPath, mode ='overwrite', *paritionColumns):
 #data bricks library required to read xml files
 def xmlWriter(processedDf, outputPath, mode ='overwrite', *paritionColumns):
     if requiredPartitions(paritionColumns):
-        processedDf.write.mode(mode).partitionBy(*paritionColumns).format('xml').save(outputPath)
+        processedDf.write.mode(mode).partitionBy(*paritionColumns).format('xml').save(outputPath, compression = codec)
     else:
         processedDf.write.mode(mode).format('xml').save(outputPath)
 
